d4fc9c6c8a8955f3996d8b04e0319aac90e316f8
Andrea Reinicke <andrea.reinicke@gmx.net>
2023-02-12 15:27:16 +0100

    Create image, name and position.

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -9,18 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.28, green: 0.49, blue: 0.69)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("myself")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 175.0, height: 175.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 3.0))
+                
+                    
+                Text("Andrea Reinicke")
+                    .font(Font.custom("Pacifico-Regular", size: 42))
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                Text("iOS Developer")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                Divider().foregroundColor(Color.white)
+            }
+            
+           
+        
+                
+            
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+                
+            
+        }
+        
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
