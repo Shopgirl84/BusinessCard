6a479df3a56e28d433c4e6cfdfa357ba5c4e689a
Andrea Reinicke <andrea.reinicke@gmx.net>
2023-02-12 21:25:40 +0100

    coming to an end. app icon ahead.

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -20,8 +20,8 @@ struct ContentView: View {
                     .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                     .overlay(Circle().stroke(Color.white, lineWidth: 3.0))
                 
-                    
-                Text("Andrea Reinicke")
+                
+                Text("Some Developer")
                     .font(Font.custom("Pacifico-Regular", size: 42))
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
@@ -29,22 +29,43 @@ struct ContentView: View {
                     .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                     .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
                 Divider().foregroundColor(Color.white)
+                
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.white)
+                    .frame(width: 383.0, height: 40.0)
+                    .overlay(HStack{
+                        Image(systemName: "phone.fill").foregroundColor(.blue)
+                        Text("555 - 123 456 789").foregroundColor(.blue)
+                    })
+                        .padding(.bottom, 5.0)
+                
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(.white)
+                            .frame(width: 383.0, height: 40.0)
+                            .overlay(HStack{
+                                Image(systemName: "envelope.fill").foregroundColor(.blue)
+                                Text("some.developer@apple.com")
+                                    .font(.body)
+                                    .foregroundColor(Color.black)
+                            })
+                            .padding(.bottom, 120.0)
+                    }
+                             
+                             
+                             
+                    
+                
             }
             
-           
-        
-                
+            
             
         }
         
-                
-            
-        }
         
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
         }
     }
-}
+
