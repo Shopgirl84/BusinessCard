44daf159538f3d8eddffaacc8b1cf300342f2704
Andrea Reinicke <andrea.reinicke@gmx.net>
2023-02-12 11:23:20 +0100

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Andrea Reinicke on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
